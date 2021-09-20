5d82d3ff8f3f40aa2af9c43cafe162d7ca557bfd
Ming <ming@localhost>
2021-09-19 20:58:05 -0700

    wget with progress and error message

--- install-texlive.sh
+++ install-texlive.sh
@@ -3,7 +3,7 @@
 # Install TeX Live, see https://www.tug.org/texlive/quickinstall.html
 #
 
-wget -q https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz || exit 1
+wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz || exit 1
 
 mkdir install-tl
 tar --strip-components 1 -zvxf install-tl-unx.tar.gz -C "$PWD/install-tl" || exit 2
